b60c4e20bf47e8cb006b17ce66d2d13bfba0a805
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-03-13 01:43:51 +0900

    環境変数周り修正/ファイル作成周り修正

--- test_run.py
+++ test_run.py
@@ -11,36 +11,29 @@ class RebuildUrlTest(unittest.TestCase):
 
     def test_ok(self):
         # 初期化
-        print("RebuildUrlTest")
         before_url = "https://pbs.twimg.com/media/hogehoge.jpg"
-        os.environ["BEARER_TOKEN"] = "sample"
-        os.environ["LIKED_USER_ID"] = "sample"
-        os.environ["DB_NAME"] = "sample"
-        os.environ["NEXT_TOKEN"] = "sample"
-        os.environ["DIR_NAME"] = "sample"
         from run import rebuild_url
         # テストの実行
         actual = rebuild_url(before_url)
         # アサーション
         self.assertEqual(
             actual, "https://pbs.twimg.com/media/hogehoge?format=png&name=large")
-        print("end")
 
 
 class BuildOutputPathTest(unittest.TestCase):
 
-    @mock.patch("run.Path")
-    @mock.patch("os.environ")
-    def test_ok(self, os_environ_mock: mock.Mock, path_mock: mock.Mock):
+    @mock.patch("run.Path.mkdir")
+    def test_ok(self, path_mock: mock.Mock):
         # 初期化
-        print("BuildOutputPathTest")
-        os_environ_mock.return_value = "sample"
         output_dir = Path.cwd()
-        created_at = datetime.datetime.now()
+        created_at = datetime.datetime(2022, 3, 13, 1)
         id = "123456789"
         index = 0
+        expect = Path.cwd() / "yyyy=2022" / "mm=03" / \
+            "dd=13" / "123456789_0.png"
         from run import make_output_path
         # テストの実行
         actual = make_output_path(output_dir, created_at, id, index)
         # アサーション
-        print(actual)
+        self.assertEqual(actual, expect)
+        self.assertEqual(len(path_mock.call_args_list), 4)
