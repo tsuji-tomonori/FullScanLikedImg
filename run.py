0a032c5ca33ce5fc3487adf77e9d5137e278f74b
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-03-12 16:38:47 +0900

    クラス化させることでグローバル変数を削除

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import time
 from pathlib import Path
+from typing import NamedTuple
 from urllib.error import HTTPError
 from urllib.request import urlopen
 
@@ -9,45 +10,43 @@ import boto3
 
 from src.twitter_api import DoseNotExistException, TwitterApi
 
-# 環境変数
-BEARER_TOKEN = os.environ["BEARER_TOKEN"]
-LIKED_USER_ID = os.environ["LIKED_USER_ID"]
-DB_NAME = os.environ["DB_NAME"]
-NEXT_TOKEN = os.environ["NEXT_TOKEN"]
-DIR_NAME = os.environ["DIR_NAME"]
+
+class EnvironParamaters(NamedTuple):
+    # 環境変数
+    BEARER_TOKEN: str
+    LIKED_USER_ID: str
+    DB_NAME: str
+    NEXT_TOKEN: str
+    DIR_NAME: str
 
 
-# boto3周り
-ssm_client = boto3.client("ssm")
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(DB_NAME)
+class AwsResource():
 
+    def __init__(self, env_param: EnvironParamaters) -> None:
+        self.env_param = env_param
+        self.ssm_client = boto3.client("ssm")
+        dynamodb = boto3.resource('dynamodb')
+        self.table = dynamodb.Table(self.env_param.DB_NAME)
 
-def get_value_from_ssm(ssm_client: boto3.client, key: str) -> str:
-    value = ssm_client.get_parameter(
-        Name=key,
-        WithDecryption=True
-    )
-    return value["Parameter"]["Value"]
+    def get_value_from_ssm(self, key: str) -> str:
+        value = self.ssm_client.get_parameter(
+            Name=key,
+            WithDecryption=True
+        )
+        return value["Parameter"]["Value"]
 
+    def put_db(self, item: dict) -> None:
+        self.table.put_item(
+            Item=item
+        )
 
-def put_db(db_resource: boto3.resource, item: dict) -> None:
-    db_resource.put_item(
-        Item=item
-    )
-
-
-def has_item(db_resource: boto3.resource, key: str) -> bool:
-    res = db_resource.get_item(
-        Key={
-            "partition_key": key
-        }
-    )
-    return bool(res.get("Item"))
-
-
-def tag_strf_query_paramater(tag: dict) -> str:
-    return "&".join(f"{k}={v}" for k, v in tag.items())
+    def has_item(self, key: str) -> bool:
+        res = self.table.get_item(
+            Key={
+                "partition_key": key
+            }
+        )
+        return bool(res.get("Item"))
 
 
 def to_jst_timezone(timestr: str, format: str) -> datetime.datetime:
@@ -120,89 +119,106 @@ def hashtags_to_str(hashtags: list) -> str:
     return ",".join(hashtag["text"] for hashtag in hashtags)
 
 
-def downdload_and_write_db(tweet_info: dict, output_dir: Path) -> None:
-    id = tweet_info["id_str"]
-    text = tweet_info["text"]
-    user_name = tweet_info["user"]["name"]
-    user_screen_name = tweet_info["user"]["screen_name"]
-    hashtag = hashtags_to_str(tweet_info["entities"]["hashtags"])
-    created_at = twitter_to_jst_timezone(tweet_info["created_at"])
-    # 1ツイート内で投稿されている画像分ループ
-    for idx, extended_entity in enumerate(tweet_info["extended_entities"]["media"]):
-        # 投稿されたメディアが画像でない場合, 次のtweetへ
-        if extended_entity["type"] != "photo":
-            continue
-        output_file = build_output_path(output_dir, created_at, id, idx)
-        # すでに取得済みであれば, 次のtweetへ
-        if has_item(table, output_file.stem):
-            print(f"skip at {output_file}")
-            continue
-        img = download_img(extended_entity["media_url_https"])
-        write_time = write_img(output_file, img)
-        print(f"write to img -> {output_file}")
-        put_db(
-            db_resource=table,
-            item={
-                "partition_key": output_file.stem,
-                "created_at": created_at.isoformat(),
-                "text": text,
-                "user_name": user_name,
-                "user_screen_name": user_screen_name,
-                "hashtag": hashtag,
-                "write_time": write_time,
-            }
-        )
+class Action():
 
+    def __init__(self, env_param: EnvironParamaters, output_dir: Path, next_token: str) -> None:
+        self._env_param = env_param
+        self._output_dir = output_dir
+        self._next_token = next_token
+        self._aws_resource = AwsResource(env_param)
 
-def service(output_dir: Path, next_token: str = None) -> None:
+    def __call__(self) -> None:
+        print(
+            f"service start! target liked user id is {self._env_param.LIKED_USER_ID}")
+        print(f"start at: {now_isof()}")
+        try:
+            self._service()
+        except Exception as e:
+            print(f"An Error occurrence at: {now_isof()}")
+            raise e
+        print(f"end at: {now_isof()}")
 
-    bearer_token = get_value_from_ssm(ssm_client, BEARER_TOKEN)
-    api = TwitterApi(bearer_token=bearer_token)
+    def _service(self) -> None:
 
-    while True:
-        ids = api.get_liked_tweets(LIKED_USER_ID, next_token)
-        # いいねが取得できなかった場合, 処理終了
-        if len(ids["data"]) == 0:
-            return
-        for data in ids["data"]:
-            try:
-                tweet_info = api.get_statuses_show(data["id"])
-            except DoseNotExistException:
-                # 詳細情報が取得できなかった場合skip
+        bearer_token = self._aws_resource.get_value_from_ssm(
+            self._env_param.BEARER_TOKEN)
+        api = TwitterApi(bearer_token=bearer_token)
+        page_token = self._next_token
+
+        while True:
+            ids = api.get_liked_tweets(
+                self._env_param.LIKED_USER_ID, page_token)
+            # いいねが取得できなかった場合, 処理終了
+            if len(ids["data"]) == 0:
+                return
+            for data in ids["data"]:
+                try:
+                    tweet_info = api.get_statuses_show(data["id"])
+                except DoseNotExistException:
+                    # 詳細情報が取得できなかった場合skip
+                    continue
+                if "extended_entities" not in tweet_info.keys():
+                    continue
+                if "media" not in tweet_info["extended_entities"].keys():
+                    continue
+                try:
+                    self._downdload_and_write_db(tweet_info, self._output_dir)
+                except Exception as e:
+                    # Twitter API 周り以外で例外が発生した場合
+                    # 先にpage_tokenを表示させる
+                    print(f"Current page token is: {page_token}")
+                    raise e
+            page_token = ids["meta"]["next_token"]
+            # Too Many Requests 対策
+            print(f"sleep at 12 sec... next token is: {page_token}")
+            time.sleep(12)
+
+    def _downdload_and_write_db(self, tweet_info: dict, output_dir: Path) -> None:
+        id = tweet_info["id_str"]
+        text = tweet_info["text"]
+        user_name = tweet_info["user"]["name"]
+        user_screen_name = tweet_info["user"]["screen_name"]
+        hashtag = hashtags_to_str(tweet_info["entities"]["hashtags"])
+        created_at = twitter_to_jst_timezone(tweet_info["created_at"])
+        # 1ツイート内で投稿されている画像分ループ
+        for idx, extended_entity in enumerate(tweet_info["extended_entities"]["media"]):
+            # 投稿されたメディアが画像でない場合, 次のtweetへ
+            if extended_entity["type"] != "photo":
                 continue
-            if "extended_entities" not in tweet_info.keys():
+            output_file = build_output_path(output_dir, created_at, id, idx)
+            # すでに取得済みであれば, 次のtweetへ
+            if self._aws_resource.has_item(output_file.stem):
+                print(f"skip at {output_file}")
                 continue
-            if "media" not in tweet_info["extended_entities"].keys():
-                continue
-            try:
-                downdload_and_write_db(tweet_info, output_dir)
-            except Exception as e:
-                # Twitter API 周り以外で例外が発生した場合
-                # 先にnext_tokenを表示させる
-                print(f"Current next token is: {next_token}")
-                raise e
-        next_token = ids["meta"]["next_token"]
-        # Too Many Requests 対策
-        print(f"sleep at 12 sec... next token is: {next_token}")
-        time.sleep(12)
-
-
-def handler(output_dir: Path, next_token: str) -> None:
-    print(f"service start! target liked user id is {LIKED_USER_ID}")
-    print(f"start at: {now_isof()}")
-    try:
-        service(
-            output_dir=output_dir,
-            next_token=next_token,
-        )
-    except Exception as e:
-        print(f"An Error occurrence at: {now_isof()}")
-        raise e
-    print(f"end at: {now_isof()}")
+            img = download_img(extended_entity["media_url_https"])
+            write_time = write_img(output_file, img)
+            print(f"write to img -> {output_file}")
+            self._aws_resource.put_db(
+                item={
+                    "partition_key": output_file.stem,
+                    "created_at": created_at.isoformat(),
+                    "text": text,
+                    "user_name": user_name,
+                    "user_screen_name": user_screen_name,
+                    "hashtag": hashtag,
+                    "write_time": write_time,
+                }
+            )
 
 
 if __name__ == "__main__":
-    handler(
-        output_dir=Path.cwd() / DIR_NAME,
-        next_token=None if NEXT_TOKEN == "" else NEXT_TOKEN,
+
+    param = EnvironParamaters(
+        BEARER_TOKEN=os.environ["BEARER_TOKEN"],
+        LIKED_USER_ID=os.environ["LIKED_USER_ID"],
+        DB_NAME=os.environ["DB_NAME"],
+        NEXT_TOKEN=os.getenv("NEXT_TOKEN", None),
+        DIR_NAME=os.environ["DIR_NAME"]
     )
+    action = Action(
+        env_param=param,
+        output_dir=Path.cwd() / param.DIR_NAME,
+        next_token=param.NEXT_TOKEN
+    )
+
+    action()
